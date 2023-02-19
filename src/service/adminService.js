97274e019abf2daab1bfb0442282c653b7bba0e7
hoangPhu123 <hoangphuletrinh@gmail.com>
2023-02-19 22:29:51 +0700

    update

--- src/service/adminService.js
+++ src/service/adminService.js
@@ -1,13 +1,26 @@
 import { https } from "./configURL";
 
+// get all users
 export const getUserList = () => {
   return https.get("/users");
 };
 
+// create a user
 export const postUser = () => {
   return https.post("/users");
 };
 
+// find a user
+export const getFindUser = () => {
+  return https.get(`/users/${id}`);
+};
+
+// update user
+export const updateUser = () => {
+  return https.patch(`/users/${id}`);
+};
+
+// delete user
 export const deleteUser = (id) => {
   return https.delete(`users/${id}`);
 };
